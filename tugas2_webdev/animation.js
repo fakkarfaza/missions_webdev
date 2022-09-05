bf099df5cdb751464ce8fce0c4ae0eec4d43b586
fakkarfaza <fakkar.keriting@gmail.com>
2022-09-05 10:41:30 +0700

    Mission 2

---
+++ tugas2_webdev/animation.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", (event) => {
+    let distance = 0
+    let direction = 1
+    let timer = setInterval(()=>{
+        cloud1.style.left = distance + "px"
+        cloud2.style.left = 750 - distance + "px"
+        cloud3.style.right = 300 - distance + "px"
+        distance = distance + 0.05 * direction
+        if (distance >= 20){
+            direction = -1
+        } else if (distance <= 0){
+            direction = 1
+        }
+    })
+})
+
+window.addEventListener("load", (event) => {
+    let angle = 1
+    let timer = setInterval(()=>{
+        rock1.style.transform = 'rotate('+ angle + 'deg)'
+        rock2.style.transform = 'rotate('+ angle + 'deg)'
+        rock3.style.transform = 'rotate('+ angle + 'deg)'
+        rock5.style.transform = 'rotate('+ angle + 'deg)'
+        angle += 0.1
+        if(angle >= 360) angle = 0;
+    })
+})
+
